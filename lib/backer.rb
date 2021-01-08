b75dd3fd67934636a5ddb640b1ce660bd7c0bd96
JC <bonassin@gmail.com>
2021-01-08 18:05:48 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,9 @@
 class Backer
-  @@backed_projects = []
+  @backed_projects = []
   attr_reader :projects
 
   def initialize
-    @@backed_projects = projects
+    @backed_projects = projects
   end
 
 end
